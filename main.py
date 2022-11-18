4d2e0b4ac933bbe528c456c29b529e9b9a8289db
Mariusz Strychowski <99507825+MSoftwaree@users.noreply.github.com>
2022-11-18 09:55:41 +0100

    Fixes for main script

--- main.py
+++ main.py
@@ -1,23 +1,22 @@
 from core.control import StabilityControl
 from core.web_page import PageGenerator
-from core.page_request import PageRequest
+from core.page_request import PageRequest, Root
 import cherrypy
 import cherrypy_cors
 import threading
 import time
 
-conf = {
-        '/': {
-            'request.dispatch': cherrypy.dispatch.MethodDispatcher(),
-            'tools.sessions.on': True,
-            'tools.response_headers.on': True,
-            'tools.response_headers.headers': [('Content-Type', 'text/plain')],
-            'cors.expose.on': True
-        }
+
+cherrypy.config.update({
+    'global': {
+        'environment': 'test_suite',
+        'server.socket_host': '192.168.67.142',
+        'server.socket_port': 8080,
+        'cors.expose.on': True
     }
+})
 
 cherrypy_cors.install()
-
 
 stab = StabilityControl()
 stab.create_stability_stations_list()
@@ -27,9 +26,13 @@ stab.activation_of_leds()
 
 page = PageGenerator(stab)
 
+cherrypy.tree.mount(Root())
+cherrypy.tree.mount(PageRequest(stab, page), '/api', {'/': {'request.dispatch': cherrypy.dispatch.MethodDispatcher()}})
+
 
 def start_requests():
-    cherrypy.quickstart(PageRequest(stab, page), '/', conf)
+    cherrypy.engine.start()
+    cherrypy.engine.block()
 
 
 requests = threading.Thread(target=start_requests)
